10b009096d87385146d8598d339735dd41d01319
Fred Chapman <areinei123@gmail.com>
2015-04-13 11:32:19 -0400

    add badges to github, shore up tests to pass

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,6 +1,5 @@
 class IngredientsController < ApplicationController
   def index
-    
     @ingredients = Ingredient.all
   end
 
@@ -8,11 +7,12 @@ class IngredientsController < ApplicationController
   @recipe = Recipe.find(params[:recipe_id])
   @ingredient = @recipe.ingredients.new(ingredient_params)
   @ingredient.user = current_user
-  if @ingredient.save
-    flash[:notice] = "ingredient Added."
-    redirect_to recipe_path(@recipe)
-  else
-    flash[:notice] = @ingredient.errors.full_messages.join("! ")
-    render 'recipes/show'
+    if @ingredient.save
+      flash[:notice] = "ingredient Added."
+      redirect_to recipe_path(@recipe)
+    else
+      flash[:notice] = @ingredient.errors.full_messages.join("! ")
+      render 'recipes/show'
+    end
   end
 end
