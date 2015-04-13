2522211a056c79b51a52402a5b085065446a2210
Fred Chapman <areinei123@gmail.com>
2015-04-13 13:40:37 -0400

    flesh out homepage

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,6 @@
 class HomesController < ApplicationController
   def index
+    @public_recipes = Recipe.where(visibility: true)
+    @user_recipes = Recipe.where(user_id: "#{current_user.id}")
   end
 end
