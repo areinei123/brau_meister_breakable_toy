e1bfeffed099b2ccbd280da019b1a393376d4a83
Fred Chapman <areinei123@gmail.com>
2015-04-15 17:04:41 -0400

    fixed travis seed issue

--- spec/features/makes_a_recipe_spec.rb
+++ spec/features/makes_a_recipe_spec.rb
@@ -16,39 +16,25 @@ feature 'Add ingredients to a recipe' do
     scenario %{I want to be able to add grains to my recipe, so that 
     i can create fermentable sugars
     } do
+      load Rails.root + "db/seeds.rb" 
       visit recipes_path
-      click_on "New Stout"
+      click_on "New Stout" 
       within("//div[@class='grain']") do
         select 'Pilsner (2 Row)', from: "list_ingredient_id"
         fill_in 'list_amount', with: 10
         click_button 'Submit'
       end
-
-      expect(page).to have_content('Pilsner (2 Row) x10.0 lbs')
-    end
-
-    scenario %{I want to be able to add hops to my recipe, so that 
-    i can make my beer more bitter and therefore more palatable
-    } do
-      visit recipes_path
-      click_on "New Stout"
       within("//div[@class='america']") do
         select 'Amarillo', from:'list_ingredient_id'
         fill_in 'list_amount', with: 3
         click_button 'Submit'
       end
-      expect(page).to have_content('Amarillo x3.0 oz.')
-    end
-
-    scenario %{I want to be able to add yeast to my recipe,
-    because i love bubbles
-    } do
-      visit recipes_path
-      click_on "New Stout"
       within("//div[@class='sour']") do
         select 'Berliner Blend GB122', from:'list_ingredient_id'
         click_button 'Submit'
       end
+      expect(page).to have_content('Amarillo x3.0 oz.')
+      expect(page).to have_content('Pilsner (2 Row) x10.0 lbs')
       expect(page).to have_content('Berliner Blend GB122')
     end
   end
