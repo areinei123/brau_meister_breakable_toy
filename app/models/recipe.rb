427ec2721a9b6a2126a7801ddbbc8956a5797724
Fred Chapman <areinei123@gmail.com>
2015-04-15 17:27:13 -0400

    removed travis.yml db:seed

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,10 @@
 class Recipe < ActiveRecord::Base
-	has_many :lists
+  has_many :lists
   has_many :ingredients, through: :lists
 
   validates :name, presence: true
   validates :batch_size, presence: true
-	belongs_to :user
+  belongs_to :user
 
   accepts_nested_attributes_for :lists
 end
