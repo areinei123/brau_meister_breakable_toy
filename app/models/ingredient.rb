13e4fcee324a6b803202374aeddf06f3ed4066d2
Keith Webber <kwebber@backupify.com>
2015-04-24 02:04:26 -0400

    added is_a_*? to quickly determine grains from, bitters and such

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,13 @@
 class Ingredient < ActiveRecord::Base
   has_many :lists
   has_many :recipes, through: :lists
+
+  def method_missing(method_sym)
+    if method_sym.to_s =~ /^is_a_(.*)?$/
+      self.type_class.downcase == $1.chomp("?")
+    else
+      super
+    end
+  end
+
 end
