9685284b58300033eb873b502688f57064ff8450
Fred Chapman <areinei123@gmail.com>
2015-04-25 14:36:59 -0400

    refactored list methods to remove logic from views

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,11 +2,14 @@ class Ingredient < ActiveRecord::Base
   has_many :lists
   has_many :recipes, through: :lists
 
+  def extract_potential_to_gu
+   (extract_potential - 1) * 1000
+  end
+
   def method_missing(method_sym)
     if method_sym.to_s =~ /^is_a_(.*)?$/
-      self.main_class == $1.chomp("?")
-    else
-      super
+      test = $1.chomp("?").capitalize
+      main_class == test
     end
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -4,4 +4,19 @@ class List < ActiveRecord::Base
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :ingredient_id, presence: true
  
+  def individual_gravity
+    ((ingredient.extract_potential_to_gu * amount) * 0.72) / recipe.batch_size
+  end
+
+  def individual_ibu_level(gravity)
+    (amount * (0.30 * boil_time) * (ingredient.alpha_acid / 100) * 7490) / (recipe.batch_size * (1 + (gravity / 0.5)))
+  end
+
+  def og_to_abv(gravity)
+    (gu_to_og(gravity) - gu_to_og(gravity * ((100 - ingredient.attenuation ) / 100))) * 131
+  end
+
+  def gu_to_og(value)
+    (value / 1000) + 1
+  end
 end
